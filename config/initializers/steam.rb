62735679fb56bf5717d287acd2eb19970076d912
David J. Brenes <davidjbrenes@gmail.com>
2015-02-21 17:28:10 +0100

    Adding an initializer for setting steam api key on development

--- config/initializers/steam.rb
+++ config/initializers/steam.rb
@@ -1,4 +1,5 @@
+STEAM_WEB_API_KEY ||= ENV['STEAM_WEB_API_KEY']
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :steam, ENV['STEAM_WEB_API_KEY'] || Rails.application.secrets.steam_api_key
+  provider :steam, STEAM_WEB_API_KEY
 end
